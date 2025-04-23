6734f57d7ac2b81a0742395fd3129aab0c2c2b90
supriya293 <supriya.perar@gmail.com>
2025-04-23 06:48:29 +0530

    Azure pipeline example

---
+++ src/main/java/myGroup/maventestazure/App.java
@@ -0,0 +1,13 @@
+package myGroup.maventestazure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! from maven.. upload to git using azure" );
+    }
+}
